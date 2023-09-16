391f201d6942f9540371bffda0a9ea2f50c2bba0
vnavani <vinita.navani@gmail.com>
2023-09-15 23:38:22 -0700

    added category-routes

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'user_id'
+  foreignKey: 'category_id'
 });
 
 // Categories have many Products
